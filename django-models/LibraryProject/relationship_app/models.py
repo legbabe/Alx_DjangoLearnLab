0864ecbdd735b3ecf2800772adcafcc61ec5f3d0
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-08-20 03:21:36 +0000

    added the query samples

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
