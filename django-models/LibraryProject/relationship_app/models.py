f564a01ddedccb00a8e7f13ac59dc51663ae9393
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-08-20 04:11:00 +0000

    added models and query samples

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,13 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length = 100)
+    
     def __str__(self):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    title = models.CharField(max_length = 100)
+    author = models.ForeignKey(Author, on_delete = models.CASCADE, related_name = 'books')
+    added_by = models.ForeignKey(User, on_delete = models.SET_NULL, null=True, related_name = 'added_books')
 
     class Meta:
         permissions = [
@@ -19,17 +21,20 @@ class Book(models.Model):
         ]
 
     def __str__(self):
-        return self.name
+        return self.title
 
 class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name='libraries')
+    name = models.CharField(max_length = 100)
+    books = models.ManyToManyField(Book, related_name = 'libraries')
+
     def __str__(self):
         return self.name
 
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = 'librarian_profile', default = 1)
+    library = models.OneToOneField(Library, on_delete = models.CASCADE)
+
     def __str__(self):
         return self.name
     
@@ -41,8 +46,8 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    role = models.CharField(max_length=10, choices = ROLE_CHOICES)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,19 +1,12 @@
-from relationship_app.models import Author, Book, Library, Librarian
+from relationship_app.models import Author
+from relationship_app.models import Library
 
-# Query all books by a specific author
-def get_books_by_author(author_name):
-    author = Author.objects.get(name = author_name)
-    books = Book.objects.filter(author = author)
-    return books
 
-# List all books in a library
-def get_books_in_library(library_name):
-    library = Library.objects.get(name = library_name)
-    books = library.books.all()
-    return books
+author = Author.objects.get(name="Author Name")
+books_by_author = author.books.all()
 
-# Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name = library_name)
-    librarian = Librarian.objects.get(library = library)
-    return librarian
+library = Library.objects.get(name="Library Name")
+books_in_library = library.books.all()
+
+library = Library.objects.get(name="Library Name")
+librarian = library.librarian
