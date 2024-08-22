540a6daf699d98530b26f02b45f8cf63e64f3cee
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-08-22 01:50:31 +0000

    done

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    """
+    A class-based view that displays details of a specific library,
+    listing all books available in that library.
+    """
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
