11ae09a6e65eb64e0ceebafc48883e82d5a25b46
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-08-22 01:53:22 +0000

    .

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -11,7 +11,8 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     
     def __str__(self):
-        return self.title 
+        return self.title  
+
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
