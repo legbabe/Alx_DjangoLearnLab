9c2368460155ca3fee3c581b4d6950dd965e1928
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-08-23 02:50:45 +0000

    .

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from .models import Library, Book 
 from django.views.generic.detail import DetailView
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
