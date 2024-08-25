f37691fb0c1737af41371933fc3391215027f1f1
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-08-25 20:58:03 +0000

    done

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,7 +7,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from forms import BookForm
 
 # Create your views here.
