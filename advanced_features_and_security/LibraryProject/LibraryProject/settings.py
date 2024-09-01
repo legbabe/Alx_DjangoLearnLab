2d183e3d3a68b34522b9f0da87928d7457f919e4
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-09-01 20:58:24 +0000

    .

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -108,6 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Custom User Model 
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
