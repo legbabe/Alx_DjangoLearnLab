5554b3aa172fed1fac0b8453581c560e430903c2
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-09-01 20:51:38 +0000

    .

--- advanced_features_and_security/LibraryProject/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/bookshelf/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import ExampleForm  # Import the form
 
-# Create your views here.
+@permission_required('bookshelf.can_add_book', raise_exception=True)
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list') 
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .forms import ExampleForm  # Import the form
 
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
-
+@permission_required('bookshelf.can_add_book', raise_exception=True)
 def example_view(request):
     if request.method == 'POST':
         form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('book_list')  # Redirect after successful form submission
+            return redirect('book_list') 
     else:
         form = ExampleForm()
 
