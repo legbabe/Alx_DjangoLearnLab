9ce1b065df095061cfa4b15d42a8d55600d881b3
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-09-25 21:46:26 +0000

    done

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,6 +10,6 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', views.user_feed, name='user-feed'),
-    path('int:pk>/like/', views.like_post, name='like_post'),
+    path('<int:pk>/like/', views.like_post, name='like_post'),
     path('<int:pk>/unlike/', views.unlike_post, name='unlike_post'),
 ]
